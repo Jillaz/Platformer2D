c4effd5b5d6af1d91f95ba52c960fafb9467a69d
Jillaz <pingwin1111@ya.ru>
2025-03-20 23:31:21 +0300

    Try5.1
    elseif

--- Assets/Scripts/ModelFlipper.cs
+++ Assets/Scripts/ModelFlipper.cs
@@ -6,15 +6,14 @@ public class ModelFlipper : MonoBehaviour
 
     private float _lookRight = 0;
     private float _lookLeft = 180;
-    
+
     public void FlipRotation(float moveDiretion)
     {
         if (moveDiretion > 0)
         {
             _viewModel.eulerAngles = new Vector2(_viewModel.rotation.x, _lookRight);
         }
-
-        if (moveDiretion < 0)
+        else if (moveDiretion < 0)
         {
             _viewModel.eulerAngles = new Vector2(_viewModel.rotation.x, _lookLeft);
         }
